474ba3850edcf014976d3efeccf3efe1a12589d9
pacee4 <165809246+pacee4@users.noreply.github.com>
2026-07-03 11:18:36 +0300

    Updates

--- assets/BufferResource-DTdOPScc.js
+++ assets/BufferResource-DTdOPScc.js
@@ -1,4 +1,4 @@
-import{w as U,p as g,S as c,u as S,B as _,q as m,E as I,r as h}from"./index-Q50gPebo.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,p as g,S as c,u as S,B as _,q as m,E as I,r as h}from"./index-CKMelV_N.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
